5e5de324bde411a821e99a452e94c7f549a2cc7f
A_Snickers_Wrapper <ameenhth@gmail.com>
2026-05-30 17:49:44 +0100

    Fixes

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 window.onload = function() {
-    console.log("Website loaded!");
-    let images = ["img1.png", "img2.png", "img3.png", "img4.png", "img5.png",
-                  "img6.png", "img7.png", "img8.png", "img9.png", "img10.png", "img11.png"];
+    console.log("JS loaded and running!");
+
+    let images = [
+        "img1.png", "img2.png", "img3.png", "img4.png", "img5.png",
+        "img6.png", "img7.png", "img8.png", "img9.png", "img10.png", "img11.png"
+    ];
+
     let index = 0;
+    const slideshow = document.getElementById("slideshow");
+
+    console.log("Slideshow element:", slideshow);
 
     function changeImage() {
         index = (index + 1) % images.length;
-        document.getElementById("slideshow").src = images[index];
+        console.log("Changing to:", images[index]);
+        slideshow.src = images[index];
     }
 
     setInterval(changeImage, 2000);
