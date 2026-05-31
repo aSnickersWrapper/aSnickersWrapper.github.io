e29cacb54800a696f8bc14c67d554f89a774eb52
A_Snickers_Wrapper <ameenhth@gmail.com>
2026-05-31 12:16:17 +0100

    WithClicker

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-window.onload = function() {
+/* CLICKER GAME */
+let score = 0;
+const clickable = document.getElementById("clickable");
+const scoreDisplay = document.getElementById("score");
 
-    // --- Slideshow 1 ---
-    let index1 = 0;
-    const track1 = document.getElementById("track1");
-    const slides1 = track1.children.length;
+// Load your sound file
+const clickSound = new Audio("jp3.mp3");
 
-    function slide1() {
-        index1 = (index1 + 1) % slides1;
-        track1.style.transform = `translateX(-${index1 * 100}%)`;
-    }
+clickable.addEventListener("click", () => {
+    score++;
+    scoreDisplay.textContent = `Clicks: ${score}`;
+    clickSound.currentTime = 0;
+    clickSound.play();
+});
 
-    setInterval(slide1, 2000);
+/* SLIDESHOW 1 */
+let index1 = 0;
+const track1 = document.getElementById("track1");
+const slides1 = track1.children.length;
 
+setInterval(() => {
+    index1 = (index1 + 1) % slides1;
+    track1.style.transform = `translateX(-${index1 * 100}%)`;
+}, 2000);
 
-    // --- Slideshow 2 ---
-    let index2 = 0;
-    const track2 = document.getElementById("track2");
-    const slides2 = track2.children.length;
+/* SLIDESHOW 2 */
+let index2 = 0;
+const track2 = document.getElementById("track2");
+const slides2 = track2.children.length;
 
-    function slide2() {
-        index2 = (index2 + 1) % slides2;
-        track2.style.transform = `translateX(-${index2 * 100}%)`;
-    }
-
-    setInterval(slide2, 2000);
-};
+setInterval(() => {
+    index2 = (index2 + 1) % slides2;
+    track2.style.transform = `translateX(-${index2 * 100}%)`;
+}, 2000);
